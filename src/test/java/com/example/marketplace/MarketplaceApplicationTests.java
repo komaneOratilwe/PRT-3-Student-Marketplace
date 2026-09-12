06f8fd019edbe590b3359293d4b1e4633cd96198
Your Name <your.email@example.com>
2026-09-12 23:30:45 +0200

    fix(test): configure MarketplaceApplicationTests to locate main application context

--- src/test/java/com/example/marketplace/MarketplaceApplicationTests.java
+++ src/test/java/com/example/marketplace/MarketplaceApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.marketplace.MarketplaceApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = MarketplaceApplication.class)
 class MarketplaceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
